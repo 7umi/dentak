4a0b4fb4ca8c6080c7cb04cb79f9908234f9b291
7umi <75528110+7umi@users.noreply.github.com>
2020-12-06 20:09:57 +0900

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,12 +133,12 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = None
-STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_ROOT = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 try:
     from .local_settings import *
